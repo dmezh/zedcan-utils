541b58835bafa2a7fc6a4434563c494cff742d3f
Dan Mezhiborsky <54985569+dmezh@users.noreply.github.com>
2022-12-14 04:45:14 -0500

    Nicer led blinky

--- zcansend/src/main.rs
+++ zcansend/src/main.rs
@@ -8,22 +8,23 @@ const BASE_GPIO_PATH: &str = "/sys/class/gpio/gpio";
 fn main() -> Result<()> {
     println!("Hello, zedcan!");
 
-    let led_nums = 1016..=1023;
+    let led_nums: Vec<u32> = (1016..=1023).collect();
 
-    let sleep_time = std::time::Duration::from_millis(200);
+    let sleep_time = std::time::Duration::from_millis(50);
 
-    for led in led_nums {
-        set_led(led, true)?;
-        sleep(sleep_time);
-        set_led(led, false)?;
-        sleep(sleep_time);
+    loop {
+        for &led in &led_nums {
+            set_led(led, true)?;
+            sleep(sleep_time);
+            set_led(led, false)?;
+            sleep(sleep_time);
+        }
     }
-
-    Ok(())
 }
 
 fn set_led(led: u32, on: bool) -> Result<()> {
     let path = format!("{BASE_GPIO_PATH}{led}/value");
 
-    fs::write(&path, (on as u32).to_string()).context(format!("Failed writing to led value at {path}"))
+    fs::write(&path, (on as u32).to_string())
+        .context(format!("Failed writing to led value at {path}"))
 }
